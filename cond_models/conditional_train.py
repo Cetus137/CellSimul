c754ab3ab838213cb7ff107a5c82969843d2b238
Ed Wheeler <edward.wheeler@kellogg.ox.ac.uk>
2025-09-01 16:15:47 +0100

    improved conditioninh

--- cond_models/conditional_train.py
+++ cond_models/conditional_train.py
@@ -7,6 +7,7 @@ conditioned on binary cell membrane segmentation masks.
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
